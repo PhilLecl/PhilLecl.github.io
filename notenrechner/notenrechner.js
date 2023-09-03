41e60390f28160604ab153ee007adfa71924f525
Philipp Leclercq <PhilLecl@users.noreply.github.com>
2023-09-03 20:42:00 +0200

    Add Footer, tooltips

--- notenrechner/notenrechner.js
+++ notenrechner/notenrechner.js
@@ -5,6 +5,7 @@ async function loadCurricula() {
     curricula = await response.json()
 }
 
+// region UI
 function initCurriculaPicker() {
     curricula.forEach((currentElement, index, arr) => {
         $("#curr-picker").append(new Option(currentElement.name, currentElement.id))
@@ -15,10 +16,14 @@ function updateModules() {
     let curriculum = curricula.filter((c) => c.id === $("#curr-picker").val())[0]
     $("#modules-container").children().remove()
     curriculum.modules.forEach((currentElement, index, arr) => {
-        $("#modules-container").append($(`<div class="row"><div class="input-group"><span class="input-group-text col-9">${currentElement.name}</span><input type="number" id="grade-${currentElement.id}" class="form-control text-end module-grade col-3" min="1.0" max="4.0" step="0.1" value="4.0"></div></div>`))
+        let title = `${currentElement.cp} CP, Faktor ${currentElement.weight}${currentElement.droppable ? "" : ", nicht streichbar"}`
+        $("#modules-container").append($(`<div class="row"><div class="input-group"><span class="input-group-text col-9" title="${title}">${currentElement.name}</span><input type="number" id="grade-${currentElement.id}" class="form-control text-end module-grade col-3" min="1.0" max="4.0" step="0.1" value="4.0"></div></div>`))
     })
 }
 
+// endregion
+
+//region grade calculation
 function makeDropCombinations(curriculum, imin = 0, dropped = 0, droppedCP = 0) {
     let drops = [[]]
     if (imin >= curriculum.modules.length) return drops
@@ -51,7 +56,7 @@ function calculateGrades() {
         dropGrades.push([drop, calculateDropGrade(curriculum, drop)])
     }
     dropGrades = dropGrades.filter((dg) => dg[1] <= dropGrades[0][1])
-    dropGrades.sort((a, b) => a[0].length === 0? 0 : a[1]-b[1] )
+    dropGrades.sort((a, b) => a[0].length === 0 ? 0 : a[1] - b[1])
     let out = ""
     for (const dg of dropGrades) {
         let line = `Gesamtnote ${(Math.floor(dg[1] * 10) / 10).toFixed(1)} (${dg[1].toFixed(4)}) `
@@ -61,6 +66,8 @@ function calculateGrades() {
     }
     $("#calc-out").val(out)
 }
+
+// endregion
 
 async function init() {
     await loadCurricula()
